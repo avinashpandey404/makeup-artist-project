a589a3d564e9b1686f6c1a908bbd526e9b8e0714
avinashpandey404 <160997057+avinashpandey404@users.noreply.github.com>
2026-05-23 10:03:57 +0530

    Create logger.py

---
+++ backend/utils/logger.py
@@ -0,0 +1,20 @@
+import logging
+
+def setup_logger():
+
+    logger = logging.getLogger("backend_logger")
+
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(levelname)s - %(message)s'
+    )
+
+    file_handler = logging.FileHandler('app.log')
+
+    file_handler.setFormatter(formatter)
+
+    if not logger.handlers:
+        logger.addHandler(file_handler)
+
+    return logger
