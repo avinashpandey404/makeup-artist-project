62fc0441120f630a4f4d52028fd65e6d463cc992
avinashpandey404 <160997057+avinashpandey404@users.noreply.github.com>
2026-05-23 10:04:20 +0530

    Create validators.py

---
+++ backend/utils/validators.py
@@ -0,0 +1,13 @@
+import re
+
+def validate_email(email):
+
+    pattern = r'^[\\w\\.-]+@[\\w\\.-]+\\.\\w+$'
+
+    return re.match(pattern, email)
+
+def validate_phone(phone):
+
+    pattern = r'^[0-9]{10}$'
+
+    return re.match(pattern, phone)
